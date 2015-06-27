700d8fb267e7000971089955d373e281c8cddbe4
Songmu <y.songmu@gmail.com>
2015-06-27 17:24:08 +0900

    initial commit

---
+++ retry_test.go
@@ -0,0 +1,42 @@
+package retry
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRetrySuccess(t *testing.T) {
+	cnt := 0
+	err := Retry(3, 1*time.Nanosecond, func() error {
+		if cnt == 0 {
+			cnt++
+			return fmt.Errorf("retry!")
+		}
+		return nil
+	})
+
+	if err != nil {
+		t.Errorf("error should be nil, %s", err)
+	}
+
+	if cnt != 1 {
+		t.Errorf("cnt should be 1, %d", cnt)
+	}
+}
+
+func TestRetryFail(t *testing.T) {
+	cnt := 0
+	err := Retry(4, 1*time.Nanosecond, func() error {
+		cnt++
+		return fmt.Errorf("retry!")
+	})
+
+	if err == nil || err.Error() != "retry!" {
+		t.Errorf("error should be occured")
+	}
+
+	if cnt != 4 {
+		t.Errorf("cnt should be 4, but %d", cnt)
+	}
+}
