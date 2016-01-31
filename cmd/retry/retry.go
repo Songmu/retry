91c3e5900493dded99675ff875363f9c8d621b93
Songmu <y.songmu@gmail.com>
2016-01-31 23:41:27 +0900

    fix interface

--- cmd/retry/retry.go
+++ cmd/retry/retry.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/Songmu/retry"
 	"github.com/Songmu/wrapcommander"
-	"github.com/jessevdk/go-flags"
 )
 
 type opts struct {
-	Retry    int64   `short:"r" long:"retry" description:"hoge"`
-	Interval float64 `short:"i" long:"interval" description:"hoge"`
+	retry    uint
+	interval float64
+	cmd      []string
 }
 
 func main() {
@@ -20,30 +22,63 @@ func main() {
 }
 
 func run(args []string) int {
-	p, o, cmdArgs, err := parseArgs(args)
-	if err != nil || len(cmdArgs) < 1 {
-		if ferr, ok := err.(*flags.Error); !ok || ferr.Type != flags.ErrHelp {
-			p.WriteHelp(os.Stderr)
+	o, err := parseArgs(args)
+	if err != nil {
+		if err.Error() != "" {
+			fmt.Fprintln(os.Stderr, err)
 		}
+		writeHelp()
 		return 2
 	}
-	err = o.run(cmdArgs)
+	err = o.run()
 	return wrapcommander.ResolveExitCode(err)
 }
 
-func parseArgs(args []string) (*flags.Parser, *opts, []string, error) {
-	o := &opts{}
-	p := flags.NewParser(o, flags.Default)
+func writeHelp() {
+	fmt.Fprint(os.Stderr, `Usage: retry COUNT [INTERVAL] COMMAND [ARG]...
 
-	rest, err := p.ParseArgs(args)
-	return p, o, rest, err
+Start COMMAND, and retry it until success up to COUNT with INTERVAL seconds.
+
+ COUNT: max retry count
+ INTERVAL: interval seconds (default 1.0)
+`)
 }
 
-func (o *opts) run(args []string) error {
-	return retry.Retry(uint(o.Retry), time.Duration(o.Interval*float64(time.Second)), func() error {
-		cmd := exec.Command(args[0], args[1:]...)
+func parseArgs(args []string) (*opts, error) {
+	if len(args) < 2 {
+		return nil, fmt.Errorf("")
+	}
+	o := &opts{}
+	retry, err := strconv.ParseUint(args[0], 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("retry COUNT should be an integer")
+	}
+	o.retry = uint(retry)
+	cmdIdx := 1
+	interval, err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		o.interval = 1.0 // default
+	} else {
+		// custom interval
+		o.interval = interval
+		cmdIdx++
+	}
+	o.cmd = args[cmdIdx:]
+	if len(o.cmd) < 1 {
+		return nil, fmt.Errorf("no command arguments are specified")
+	}
+	return o, nil
+}
+
+func (o *opts) run() error {
+	return retry.Retry(uint(o.retry), time.Duration(o.interval*float64(time.Second)), func() error {
+		cmd := exec.Command(o.cmd[0], o.cmd[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		return cmd.Run()
+		err := cmd.Run()
+		if !wrapcommander.IsInvoked(err) {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return err
 	})
 }
