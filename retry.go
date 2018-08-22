0e6a0cd4b8f226b7fc5dabc2af91dde5aa791f2b
itchyny <itchyny@hatena.ne.jp>
2018-08-22 10:53:41 +0900

    implement RetryWithContext

--- retry.go
+++ retry.go
@@ -1,6 +1,9 @@
 package retry
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 // Retry calls the `fn` and if it returns the error, retry to call `fn` after `interval` duration.
 // The `fn` is called up to `n` times.
@@ -14,4 +17,21 @@ func Retry(n uint, interval time.Duration, fn func() error) (err error) {
 		time.Sleep(interval)
 	}
 	return err
+}
+
+// RetryWithContext stops retrying when the context is done.
+func RetryWithContext(ctx context.Context, n uint, interval time.Duration, fn func() error) (err error) {
+	for n > 0 {
+		n--
+		err = fn()
+		if err == nil || n <= 0 {
+			break
+		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(interval):
+		}
+	}
+	return
 }
