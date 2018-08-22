b13be4a37fcdf402abb264be746cb5d04a77b30b
Songmu <y.songmu@gmail.com>
2018-08-22 12:31:08 +0900

    using `WithContext` in `Retry`

--- retry.go
+++ retry.go
@@ -8,15 +8,7 @@ import (
 // Retry calls the `fn` and if it returns the error, retry to call `fn` after `interval` duration.
 // The `fn` is called up to `n` times.
 func Retry(n uint, interval time.Duration, fn func() error) (err error) {
-	for n > 0 {
-		n--
-		err = fn()
-		if err == nil || n <= 0 {
-			break
-		}
-		time.Sleep(interval)
-	}
-	return err
+	return WithContext(context.Background(), n, interval, fn)
 }
 
 // WithContext stops retrying when the context is done.
